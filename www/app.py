87fd486a7412b211015dbdae175b378ea9d1edd5
dzx_home <754958439@qq.com>
2018-03-01 22:50:01 +0800

    给代码增加了一些注释

--- www/app.py
+++ www/app.py
@@ -1,11 +1,10 @@
 import logging
-
-logging.basicConfig(level=logging.INFO)
-
 import asyncio, os, json, time
 from datetime import datetime
 
 from aiohttp import web
+
+logging.basicConfig(level=logging.INFO)
 
 
 def index(request):
